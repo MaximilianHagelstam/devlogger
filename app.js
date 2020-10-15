c2536ce60e8495d8898660a4a954456b8017d533
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2020-10-15 11:26:08 +0300

    added express routing and bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const port = 3000;
+const port = 3000
 
-app.listen(port);
+app.listen(port)
 
-app.gety("/", (req, res) => {
-  res.send("");
-});
+app.get('/', (req, res) => {
+	res.sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/about', (req, res) => {
+	res.sendFile('./views/about.html', { root: __dirname })
+})
+
+// Redirect
+app.get('/about-us', (req, res) => {
+	res.redirect('/about')
+})
+
+// 404
+app.use((req, res) => {
+	res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
