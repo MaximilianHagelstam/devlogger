d186e33cd342ab8c6dc4ca24966a640d5698c742
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2020-10-16 14:04:22 +0300

    designed all pages and added express routing

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
 const express = require('express')
 
+// Express app
 const app = express()
+
+// Register view engine
+app.set('view engine', 'ejs')
 
 const port = 3000
 
-app.listen(port)
+// Listen for requests
+app.listen(port, () => {
+	console.log('Listening on port ' + port)
+})
 
+// Routes
 app.get('/', (req, res) => {
-	res.sendFile('./views/index.html', { root: __dirname })
+	// Posts
+	const posts = [
+		// {
+		// 	title: 'Yoshi finds eggs',
+		// 	body: 'Lorem ipsum dolor sit amet consectetur',
+		// },
+		// {
+		// 	title: 'Mario finds stars',
+		// 	body: 'Lorem ipsum dolor sit amet consectetur',
+		// },
+		// {
+		// 	title: 'How to defeat bowser',
+		// 	body: 'Lorem ipsum dolor sit amet consectetur',
+		// },
+	]
+
+	res.render('index', { title: 'Home', posts })
 })
 
 app.get('/about', (req, res) => {
-	res.sendFile('./views/about.html', { root: __dirname })
+	res.render('about', { title: 'About' })
 })
 
-// Redirect
+app.get('/compose', (req, res) => {
+	res.render('compose', { title: 'Compose' })
+})
+
+app.get('/signin', (req, res) => {
+	res.render('signin', { title: 'Sign in' })
+})
+
+// Redirects
 app.get('/about-us', (req, res) => {
 	res.redirect('/about')
 })
 
+app.get('/home', (req, res) => {
+	res.redirect('/')
+})
+
 // 404
 app.use((req, res) => {
-	res.status(404).sendFile('./views/404.html', { root: __dirname })
+	res.status(404).render('404', { title: 'Page not found' })
 })
